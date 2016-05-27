8c63f3cb547b7105fa11c39069bd1240ecb528ab
Hugo Marcotte <marcotte.hugo@gmail.com>
2016-05-27 10:46:29 -0400

    React rails gem

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,9 @@
 //= require jquery_ujs
 //= require my_bookmarks
 //= require turbolinks
+//= require react
+//= require react_ujs
+//= require components
 //= require_tree .
 
 (function($, undefined) {
